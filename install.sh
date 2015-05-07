0e7278d1409b61baf0ee42a01e674011fc73a5ac
Eric Villard <dev@eviweb.fr>
2015-05-07 15:52:31 +0200

    add installer
    Signed-off-by: Eric Villard <dev@eviweb.fr>

---
+++ install.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+DIR=$(dirname $(readlink -f "$0"))
+INITD=/etc/init.d
+UNINSTALL=0
+
+# installer usage
+usage() {
+    echo "
+    Usage:
+        ./install.sh [OPTIONS]
+
+    Options:
+        -u      uninstall docker-registry start-up script
+        -h      display this message
+
+    Install/uninstall docker-registry start-up script
+"
+}
+
+# check for root privileges
+check_root() {
+    if [ `whoami` != "root" ]
+    then
+        echo "Please grant root privileges before running this script..."
+        exit 1
+    fi
+}
+
+# install the start-up script
+install() {
+    ln -s ${DIR}/docker-registry ${INITD}/
+    update-rc.d docker-registry defaults
+}
+
+# uninstall the start-up script
+uninstall() {
+    unlink ${INITD}/docker-registry
+    update-rc.d docker-registry remove
+}
+
+OPTIONS=":hsu"
+# get command line options
+while getopts $OPTIONS option
+do
+    case $option in
+        u) UNINSTALL=1;;
+        *) usage && exit 1;;
+    esac
+done
+shift $(($OPTIND - 1 ))
+
+check_root
+
+if [ ${UNINSTALL} -eq 0 ]
+then
+    install
+else
+    uninstall
+fi
